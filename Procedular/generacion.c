5c791adab02b855096ccddb5f949b7f55703ca34
Paco <curro258@gmail.com>
2018-09-25 03:52:35 +0200

    estilo

--- Procedular/generacion.c
+++ Procedular/generacion.c
@@ -14,25 +14,20 @@ Con seguridad sabes que deberás reservar una variable entera para guardar
 el puntero de pila extendido (esp). 
 Se te sugiere el nombre __esp para esta variable.
 */
-void escribir_cabecera_bss(FILE* fpasm){
+void escribir_cabecera_bss(FILE* fpasm) {
 	char definicion[] = "segment .bss";
 	char puntero_pila[] = "\t__esp resd 1"
 
-	if (!fpasm){
+	if (!fpasm) {
 		printf("Error de fichero (escribir_cabecera_bss)\n");
-	} 
+	} else {
 
-	else{
-
-	/** Escribimos en el fichero la definición del segmento .bss e inicializamos el puntero de pila*/
-		if (fprintf(fpasm, "%s\n", definicion) > 0){
-
-			if (fprintf(fpasm, "%s\n", puntero_pila) <= 0){
+		/** Escribimos en el fichero la definición del segmento .bss e inicializamos el puntero de pila*/
+		if (fprintf(fpasm, "%s\n", definicion) > 0) {
+			if (fprintf(fpasm, "%s\n", puntero_pila) <= 0) {
 				printf("Error inicializando el puntero de pila\n");
 			}
-		}	
-
-		else {
+		} else {
 			printf("Error definiendo el segmento .bss\n");
 		}
 	}
@@ -48,12 +43,11 @@ void escribir_cabecera_bss(FILE* fpasm){
 void escribir_subseccion_data(FILE* fpasm){
 	char mensaje[] = "segment.data\n\tmsg_error_division_por_0\tdb \"Division por 0\", 0"
 
-	if (!fpasm){
+	if (!fpasm) {
 		printf("Error de fichero (escribir_subsection_data)\n");
-	} 
+	} else {
 
-	else{
-		if (fprintf(fpasm, "%s\n", mensaje) < 0){
+		if (fprintf(fpasm, "%s\n", mensaje) < 0) {
 			printf("Error declarando subseccion data\n");
 		}
 	}	
@@ -70,17 +64,16 @@ Esta misma función se invocará cuando en el compilador se declaren vectores, p
 (tamano) que para esta primera práctica siempre recibirá el valor 1. */
 
 void declarar_variable(FILE* fpasm, char * nombre,  int tipo,  int tamano){
-	/**Variable donde guardaremos la declaracion de la variable en ensamblador*/
+	/** Variable donde guardaremos la declaracion de la variable en ensamblador*/
 	char *declaracion = "";
 
-	if (!fpasm){
+	if (!fpasm) {
 		pritnf("Error de fichero (declarar_variable");
-	}
-
-	else{
-		sprintf (declaracion, "\t_%s resd %d", nombre, tamano);
+	} else {
+		sprintf(declaracion, "\t_%s resd %d", nombre, tamano);
 		fprintf(fpasm, "%s\n", declaracion);
 	}
+	
 	return;
 }
 
