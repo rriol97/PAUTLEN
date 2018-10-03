45e7f96397db21255f2dbfa364a49236fdec3948
mighark <36306642+mighark@users.noreply.github.com>
2018-10-03 17:37:16 +0200

    Add files via upload
    Hash

---
+++ OO/hash.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "uthash.h"
+#include "hash.h"
+
+
+void insert_symbol(Hash **hash, char* name, void* value) {
+    Hash *s;
+
+    s = malloc(sizeof(Hash));
+    s->value = value;
+    strcpy(s->name, name);
+    HASH_ADD_INT( *hash, name, s );
+}
+
+void delete_symbol(Hash **hash, char* name){
+    Hash *point = NULL;
+
+    HASH_FIND_PTR( *hash, &name, point );
+   
+    HASH_DEL(*hash, point);  
+    free(point);             
+}
+
+void* find_symbol(Hash **hash, char* name){
+    Hash *point = NULL;
+
+    HASH_FIND_PTR( *hash, &name, point );
+    return point->value;
+
+}
+
+void clear_symbols(Hash **hash){
+  struct Hash *point, *tmp;
+
+  HASH_ITER(hh, *hash, point, tmp) {
+  HASH_DEL(*hash,point);  
+  free(point);            
+  }
+}
+
+
---
+++ OO/hash.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "uthash.h"
+
+typedef struct _Hash{
+	char* name;
+	void* value;
+	UT_hash_handle hh
+	}Hash;
+
+
+void insert_symbol(Hash **hash, char* name, void* value);
+
+void delete_symbol(Hash **hash, char* name);
+
+void* find_symbol(Hash **hash, char* name);
+
+void clear_symbols(Hash **hash);
+
+
