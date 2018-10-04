4133f3ff9e177124e00eb48e2dcdf2c1a57f62e7
mighark <miguel.garciamoya@estudiante.uam.es>
2018-10-04 14:23:16 +0200

    Comprobación de errores y .h del grafo

--- OO/graph.h
+++ OO/graph.h
@@ -5,12 +5,23 @@
 #define MAX_NAME 100
 
 typedef struct {
-	int index;
-	char name[MAX_NAME];
-	/*TablaAmbito tabla;*/
+    int index;
+    char name[MAX_NAME];
+    /*TablaAmbito tabla;*/
 } Class;
 
 typedef struct _Graph Graph;
 
+Graph* createGraph();
+
+void freeGraph(Graph* graph);
+
+/*0 if succesful, -1 if error*/
+int addEdge(Graph* graph, int src, int dest);
+
+/*0 if succesful, -1 if error*/
+int addClass(Graph* graph, Class * class);
+
+Class* getClass(Graph* graph, int index);
 
 #endif
